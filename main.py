f72ec4328b1bb97d181c3653b21444f4bb1a1aae
jeff <jeff.etale@student.moringaschool.com>
2023-09-04 17:00:40 +0300

    decdecided to se main.py instead of seed.py to update the database

--- main.py
+++ main.py
@@ -2,22 +2,29 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from config import SQLALCHEMY_DATABASE_URI
 
-
 engine = create_engine(SQLALCHEMY_DATABASE_URI)
 
 Session = sessionmaker(bind=engine)
 session = Session()
 
 if __name__ == "__main__":
-    from app.models import Customer  # Import your models
+
+    
+    # Import models
+    from app.models import Customer  
 
     # Create a new customer instance
     new_customer = Customer(first_name="John", last_name="Doe")
+    new_customer_1 = Customer(first_name="Gib", last_name="Raltar")
 
     # Add the customer to the session and commit to the database
     session.add(new_customer)
+    session.add(new_customer_1)
     session.commit()
 
     # Verify that the customer has been added
     john_doe = session.query(Customer).filter_by(first_name="John", last_name="Doe").first()
     print(f"New customer ID: {john_doe.id}")
+    
+    gib_raltar = session.query(Customer).filter_by(first_name="Gib", last_name="Raltar").first()
+    print(f"New customer ID: {gib_raltar.id}")
